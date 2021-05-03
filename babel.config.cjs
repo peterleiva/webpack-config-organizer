45ec98fdab34353f92860af9942fc916109e1d0b
Peter Leiva <pleiva@id.uff.br>
2021-05-03 08:48:33 -0300

    chore: add babel config

---
+++ babel.config.cjs
@@ -0,0 +1,8 @@
+/**
+ * @type {import('@babel/core').ConfigFunction}
+ */
+module.exports = api => ({
+	retainLines: true,
+	sourceMaps: api.env("test") ? "inline" : false,
+	presets: [["@babel/preset-env", { targets: { node: "current" } }]],
+})
