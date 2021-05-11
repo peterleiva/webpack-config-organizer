a1b3653cf5d58ca19f03a216093d7086dedf9d49
Peter Leiva <pleiva@id.uff.br>
2021-05-11 16:38:03 -0300

    test: fix

--- jest.config.js
+++ jest.config.js
@@ -6,8 +6,15 @@
 export default {
 	collectCoverage: true,
 	coverageDirectory: "coverage",
-	coveragePathIgnorePatterns: ["/node_modules/", "/dist/"],
+	coveragePathIgnorePatterns: ["/node_modules/", "/dist/", ".vscode", ".husky"],
 	coverageProvider: "v8",
+	coverageThreshold: {
+		global: {
+			lines: 90,
+			statements: -10,
+			branches: -20,
+		},
+	},
 
 	testEnvironment: "node",
 
